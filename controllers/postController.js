5d8d9863bd9fe7f2d0c52fe10bb6abd7336277e6
Carlo Costa <carlo.costa92@gmail.com>
2025-05-08 14:37:50 +0200

    feat: milestone 1

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,22 +1,23 @@
-const posts= require("../data/posts.js")
+const posts = require("../data/posts.js")
+const connection = require("../data/db.js")
 
 const index = (req, res) => {
-    const check=req.query.tag;
+    const check = req.query.tag;
 
-    if (check){
+    if (check) {
         // Filtra i post per tag
-    const checkedPosts = posts.filter((post) =>
-        post.tags.some((tag) => tag === check)
-      );
-      if (checkedPosts.length === 0) {
-        return res.status(404).json({
-          error: "not Found",
-          messaggio: "Nessun post trovato ",
-        });
-      }
-  
-      res.json(checkedPosts); //stampa quelli trovati
-    
+        const checkedPosts = posts.filter((post) =>
+            post.tags.some((tag) => tag === check)
+        );
+        if (checkedPosts.length === 0) {
+            return res.status(404).json({
+                error: "not Found",
+                messaggio: "Nessun post trovato ",
+            });
+        }
+
+        res.json(checkedPosts); //stampa quelli trovati
+
     } else {
         res.json(posts); //stampa tutto
     }
@@ -28,28 +29,29 @@ const show = (req, res) => {
     // validazione
     if (req.params.id) {
 
-    const postId = parseInt(req.params.id);
-    const post = posts.find(post => post.id === postId);
+        const postId = parseInt(req.params.id);
+        const post = posts.find(post => post.id === postId);
 
-    if (!post) {
-        return res.status(404).json({ 
-            error: "not found",
-            messaggio: "ricetta non trovata" });
-      }
+        if (!post) {
+            return res.status(404).json({
+                error: "not found",
+                messaggio: "ricetta non trovata"
+            });
+        }
 
-    // res.send('Dettagli di un post');
-    res.status(200).json(post);
-}
+        // res.send('Dettagli di un post');
+        res.status(200).json(post);
+    }
 };
 
 const store = (req, res) => {
     console.log(req.body);
 
     //variabile per nuovo id sequenziale
-    const newId= posts[posts.length-1].id + 1;
+    const newId = posts[posts.length - 1].id + 1;
 
     // creazione oggetto
-    const newRecipe={
+    const newRecipe = {
         id: newId,
         title: req.body.title,
         content: req.body.content,
@@ -70,28 +72,28 @@ const store = (req, res) => {
 const update = (req, res) => {
 
     //recuperiamo id dall'url
-    const id=parseInt(req.params.id);
+    const id = parseInt(req.params.id);
 
     //cerchiamo l'elemento
-    const recipe= posts.find(recipe => recipe.id === id);
+    const recipe = posts.find(recipe => recipe.id === id);
 
     //validazione
-    if(!recipe){
+    if (!recipe) {
         return res.status(404).json({
             error: "not found",
             messaggio: "ricetta non trovata"
         })
     } else {
 
-    //aggiorniamo il ricettario
-    recipe.title=req.body.title;
-    recipe.content=req.body.content;
-    recipe.image=req.body.image;
-    recipe.tags=req.body.tags;
+        //aggiorniamo il ricettario
+        recipe.title = req.body.title;
+        recipe.content = req.body.content;
+        recipe.image = req.body.image;
+        recipe.tags = req.body.tags;
 
-    console.log(posts)
+        console.log(posts)
 
-    res.json(recipe);
+        res.json(recipe);
 
     }
 
@@ -106,10 +108,10 @@ const destroy = (req, res) => {
     //validazione
     if (post === -1) {
         return res.status(404).json({
-          error: "Not Found",
-          messaggio: "Post non trovato.",
+            error: "Not Found",
+            messaggio: "Post non trovato.",
         });
-      }
+    }
 
     posts.splice(post, 1);
 
